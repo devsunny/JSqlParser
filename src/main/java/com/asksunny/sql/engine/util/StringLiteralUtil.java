706aabfaa428fb006b74fa4313b170c15b9f4c74
Developer Sunny <sunnyliu2@gmail.com>
2015-02-03 08:32:57 -0500

    Add StringLiteralUtil

---
+++ src/main/java/com/asksunny/sql/engine/util/StringLiteralUtil.java
@@ -0,0 +1,53 @@
+package com.asksunny.sql.engine.util;
+
+public final class StringLiteralUtil {
+
+	public static final String RUBY_TEXT_QUOTE = "\"\"\"";
+	public static final String SCRIPT_SINGLE_QUOTE = "'";
+	public static final String STRING_DOUBLE_QUOTE = "\"";
+	public static final int QUOTE_LEN_1 = 1;
+	public static final int QUOTE_LEN_3 = 3;
+
+	private StringLiteralUtil() {
+	}
+
+	public String unquote(String value) {
+		if (value == null) {
+			return null;
+		} else if (value.startsWith(STRING_DOUBLE_QUOTE)
+				&& value.endsWith(STRING_DOUBLE_QUOTE)) {
+			return value.substring(QUOTE_LEN_1, value.length() - QUOTE_LEN_1);
+		} else if (value.startsWith(SCRIPT_SINGLE_QUOTE)
+				&& value.endsWith(SCRIPT_SINGLE_QUOTE)) {
+			return value.substring(QUOTE_LEN_1, value.length() - QUOTE_LEN_1);
+		} else if (value.startsWith(RUBY_TEXT_QUOTE)
+				&& value.endsWith(RUBY_TEXT_QUOTE)) {
+			return value.substring(QUOTE_LEN_3, value.length() - QUOTE_LEN_3);
+		} else {
+			return value;
+		}
+	}
+
+	public String unquote(String value, String quote) {
+		if (value == null) {
+			return null;
+		} else if (value.startsWith(quote) && value.endsWith(quote)) {
+			return value.substring(quote.length(),
+					value.length() - quote.length());
+		} else {
+			return value;
+		}
+	}
+	
+	public String unquote(String value, String startQuote, String endQuote) {
+		if (value == null) {
+			return null;
+		} else if (value.startsWith(startQuote) && value.endsWith(endQuote)) {
+			return value.substring(startQuote.length(),
+					value.length() - endQuote.length());
+		} else {
+			return value;
+		}
+	}
+
+}
